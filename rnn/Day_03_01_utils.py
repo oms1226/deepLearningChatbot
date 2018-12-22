406d332b83b618b850ab4f2160b329e573722a00
오명섭 <oms1226@gmail.com>
2018-12-22 09:37:52 +0900

    python lamda 설명

--- rnn/Day_03_01_utils.py
+++ rnn/Day_03_01_utils.py
@@ -8,6 +8,7 @@ def proxy(func, n):
     print(func)
     print(func(n))
 
+lamb = lambda n: n * 2 ##return 이 생략된 한줄짜리 함수
 
 f = twice
 print(f)
@@ -18,3 +19,8 @@ print(f(3))
 
 #proxy(3)
 proxy(twice, 3)
+
+print(lamb(7))
+print((lambda n: n * 2)(7))
+
+proxy(lambda n: n * 2, 3)
